acd268b6e072f1b5ef7f94477224d19c10eafde7
Henzo-Brito <henzo.2025@proton.me>
2026-08-14 22:02:47 -0300

    V-0.0.2

---
+++ src/script/btns.js
@@ -0,0 +1,37 @@
+const menuBtn = document.getElementById("menuBtn");
+const navBar = document.getElementById("navBar");
+const btnTheme = document.getElementById("btnTheme");
+
+let activated = false
+
+menuBtn.addEventListener("click", () => {
+    activated = !activated;
+
+    navBar.classList.toggle("active", activated);
+    menuBtn.classList.toggle("active", activated);
+});
+
+
+btnTheme.addEventListener("click", () => {
+    activated = !activated;
+
+    document.documentElement.dataset.theme =
+        activated ? "light" : "dark";
+
+    btnTheme.classList.toggle("fa-sun", !activated);
+    btnTheme.classList.toggle("fa-moon", activated);
+
+        const images = document.querySelectorAll("img");
+
+    images.forEach((img) => {
+        if (activated) {
+            img.src = img.src
+                .replace("logo.png", "logo-dark.png")
+                .replace("logotipo.png", "logotipo-dark.png");
+        } else {
+            img.src = img.src
+                .replace("logo-dark.png", "logo.png")
+                .replace("logotipo-dark.png", "logotipo.png");
+        }
+    });
+});
